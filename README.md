2e12d8a4d13f383967f26b174ceac40224d402e1
renatodellosso <rddellosso4@gmail.com>
2024-12-23 14:27:26 -0500

    Initial release

--- README.md
+++ README.md
@@ -1,16 +1,96 @@
-# NPM Package Template
+# Easy API
 
-To get started, use this repo as a template and do the following:
+Easy API lets you create APIs quickly and easily.
 
-- Update the details in package.json
-- Update the assignees in .github/dependabot.yml
-- Add your NPM_TOKEN to the repository secrets (needed to publish to NPM)
-- Add your GIT_PUSH_TOKEN to the repository secrets (needed to auto-increment the version number)
+## Usage
 
-Branch protection is set up for the main branch, requiring you to PR into main, have at least one review, and pass CI checks. Creating a PR will also automatically increment the patch number.
+### Creating endpoints
 
-Comes with ESLint, Prettier, and Jest set up. To run tests, use `npm test`. To run linting, use `npm run lint`. To run prettier, use `npm run format`.
+To get started, create a new subclass of `ApiTemplate<TDependencies>` and a type for your API's external dependencies,
+like so:
 
-Commits to main are automatically published to NPM (provided your CI checks pass and you've changed the package name).
+```typescript
+type TestDependencies = {
+	testDependency: string; // Pretend we have to fetch this from our database
+};
 
-NPM Package Template © 2024 by Decatur Robotics is licensed under the MIT license.
+class TestApi extends ApiTemplate<TestDependencies> {
+	constructor() {
+		// First argument is the prefix for all API routes, second argument is a function that is called whenever a route throws an error.
+		const requestHelper = new RequestHelper(API_PREFIX, () => {});
+		super(requestHelper, false);
+		this.init(); // Init configures API routes so they are ready to be called.
+	}
+}
+```
+
+Then, create a field in your API class for each endpoint, like so:
+
+```typescript
+class TestApi extends ApiTemplate<TestDependencies> {
+	rootRoute = createRoute<[string, number], string, TestDependencies, {}>({
+		isAuthorized: (req, res, deps, [name, number]) =>
+			Promise.resolve({ authorized: true, authData: {} }),
+
+		handler: (req, res, deps, authData, [name, number]) => {
+			res.status(200).send(`Hello, ${name} ${number}!`);
+		},
+	});
+}
+```
+
+`createRoute` is the function that creates an endpoint. It's generic parameters, in order, are:
+
+- The type of the arguments that the endpoint takes, as an array. Ex: `[string, number]`
+- The type of the return value of the endpoint. Ex: `string`
+- The type of the dependencies that the endpoint uses. This should be the same as the `TDependency` generic parameter you passed to `ApiTemplate`. Ex: `TestDependencies`
+- The type of the data that is fetched in the process of determining whether the request is authorized. Ex: `undefined`, `{}`
+
+It takes an object with two fields: `isAuthorized` and `handler`.
+
+`isAuthorized` is a function that determines whether the request is authorized. It takes the request, response, dependencies,
+and arguments provided by the API call. It should return a promise that resolves to an object with two fields: `authorized`
+and `authData`. `authData` is any data that was fetched in the process of determining whether the request is authorized.
+
+`handler` is a function that handles the request. It takes the request, response, dependencies, authData, and arguments provided
+by the API call. Use `res.status(code)` and `res.send(obj)` to respond to API requests.
+
+### Responding to requests
+
+Finally, create an subclass of `ServerApi<TDependencies>`, pass your API class and error logging mode (throw, log, or none) to the super constructor, and override the `getDependencies` method, like so:
+
+```typescript
+class TestServerApi extends ServerApi<TestDependencies> {
+	constructor() {
+		super(new TestApi(), ErrorLogMode.None);
+	}
+
+	getDependencies() {
+		return {
+			testDependency: "test",
+		};
+	}
+}
+```
+
+`getDependencies` must return an object of type `TDependencies`.
+
+### Connecting your API to a web server
+
+To connect your web server to your `ServerApi`, call the `handle` method and pass in the request and the response objects, like so:
+
+```typescript
+const serverApi = new TestServerApi();
+await serverApi.handle(req, res); // await is optional
+```
+
+### Calling your API
+
+To call your API, create an instance of your `ApiTemplate` subclass and call the appropriate method, like so:
+
+```typescript
+const clientApi = new TestApi();
+await clientApi.requestHelper("Test", 1234); // await is optional
+```
+
+## Easy API © 2024 by Decatur Robotics is licensed under the MIT license.
---
+++ tests/index.test.ts
@@ -0,0 +1,163 @@
+import {
+	ApiResponse,
+	ApiTemplate,
+	createRoute,
+	ErrorLogMode,
+	RequestHelper,
+	ServerApi,
+} from "../src";
+
+const API_PREFIX = "api/";
+
+type TestDependencies = {
+	testDependency: string;
+};
+
+export class TestRes implements ApiResponse<any> {
+	status = jest.fn((code) => this);
+	send = jest.fn((obj) => this);
+	error = jest.fn((code, message) => {
+		this.status(code);
+		this.send({ error: message });
+		return this;
+	});
+}
+
+class TestApi extends ApiTemplate<TestDependencies> {
+	segment = {
+		routeWithPresetCaller: createRoute<[string, number], string, TestDependencies, {}>(
+			{
+				isAuthorized: (req, res, deps, [name, number]) =>
+					Promise.resolve({ authorized: true, authData: {} }),
+				handler: (req, res, deps, authData, [name, number]) => {
+					res.status(200).send(`Hello, ${name} ${number}!`);
+				},
+			},
+			(name: string, number: number) => {
+				return Promise.resolve(`Hello, ${name} ${number}!`);
+			},
+		),
+
+		routeWithoutPresetCaller: createRoute({
+			isAuthorized: (req, res, deps, [name, number]) =>
+				Promise.resolve({ authorized: true, authData: {} }),
+			handler: (req, res, deps, authData, [name, number]) => {
+				res.status(200).send(`Hello, ${name} ${number}!`);
+			},
+		}),
+	};
+
+	rootRoute = createRoute({
+		isAuthorized: (req, res, deps, [name, number]) =>
+			Promise.resolve({ authorized: true, authData: {} }),
+		handler: (req, res, deps, authData, [name, number]) => {
+			res.status(200).send(`Hello, ${name} ${number}!`);
+		},
+	});
+
+	unauthorizedRoute = createRoute({
+		isAuthorized: (req, res, deps, args) =>
+			Promise.resolve({ authorized: false, authData: undefined }),
+		handler(req, res, deps, authData, args) {
+			res.status(200).send("Should not be happening!");
+		},
+	});
+
+	routeWithAuthData = createRoute({
+		isAuthorized: (req, res, deps, args) =>
+			Promise.resolve({ authorized: true, authData: { x: 0 } }),
+		handler(req, res, deps, authData, args) {
+			res.status(200).send(authData.x);
+		},
+	});
+
+	constructor() {
+		const requestHelper = new RequestHelper(API_PREFIX, () => {});
+		super(requestHelper, false);
+		this.init();
+	}
+}
+
+class TestServerApi extends ServerApi<TestDependencies> {
+	constructor() {
+		super(new TestApi(), ErrorLogMode.None);
+	}
+
+	getDependencies() {
+		return {
+			testDependency: "test",
+		};
+	}
+}
+
+const clientApi = new TestApi();
+
+test(`ApiLib.${createRoute.name}: Creates callable route`, async () => {
+	const res = await clientApi.segment.routeWithPresetCaller("world", 42);
+	expect(res).toBe("Hello, world 42!");
+});
+
+test(`ApiLib.${ApiTemplate.name}.init: Sets subUrl`, () => {
+	expect(clientApi.segment.routeWithPresetCaller.subUrl).toBe(
+		"/segment/routeWithPresetCaller",
+	);
+});
+
+test(`ApiLib.${ApiTemplate.name}.init: Sets caller`, async () => {
+	expect(clientApi.segment.routeWithPresetCaller.call).toBeDefined();
+});
+
+test(`ApiLib.${createRoute.name}: Creates callable route without caller`, async () => {
+	expect(typeof clientApi.segment.routeWithoutPresetCaller).toBe("function");
+	expect(clientApi.segment.routeWithoutPresetCaller.subUrl).toBe(
+		"/segment/routeWithoutPresetCaller",
+	);
+});
+
+test(`ApiLib.${ServerApi.name}.${ServerApi.prototype.handle.name}: Finds correct method`, async () => {
+	const req = {
+		url: API_PREFIX + "segment/routeWithoutPresetCaller",
+		body: ["world", 42],
+	};
+	const res = new TestRes();
+
+	await new TestServerApi().handle(req as any, res as any);
+
+	expect(res.send).toHaveBeenCalledWith("Hello, world 42!");
+	expect(res.status).toHaveBeenCalledWith(200);
+});
+
+test(`ApiLib.${ServerApi.name}.${ServerApi.prototype.handle.name}: Finds methods that are not in segments`, async () => {
+	const req = {
+		url: API_PREFIX + "rootRoute",
+		body: ["world", 42],
+	};
+	const res = new TestRes();
+
+	await new TestServerApi().handle(req as any, res as any);
+
+	expect(res.send).toHaveBeenCalledWith("Hello, world 42!");
+	expect(res.status).toHaveBeenCalledWith(200);
+});
+
+test(`ApiLib.${ServerApi.name}.${ServerApi.prototype.handle.name}: Throws 403 if unauthorized`, async () => {
+	const req = {
+		url: API_PREFIX + "unauthorizedRoute",
+	};
+	const res = new TestRes();
+
+	await new TestServerApi().handle(req as any, res as any);
+
+	expect(res.status).toHaveBeenCalledWith(403);
+});
+
+test(`ApiLib.${ServerApi.name}.${ServerApi.prototype.handle.name}: Passes authData to handler`, async () => {
+	const req = {
+		url: API_PREFIX + "routeWithAuthData",
+	};
+	const res = new TestRes();
+
+	await new TestServerApi().handle(req as any, res as any);
+
+	expect(res.send).toHaveBeenCalledWith(0);
+});
